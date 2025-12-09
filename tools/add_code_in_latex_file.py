ce0e0017138d311562078f86f1effe895dfbae82
Gerrald <gerrald@weelden.net>
2025-12-09 14:16:30 +0100

    Updated latex code generator

--- tools/add_code_in_latex_file.py
+++ tools/add_code_in_latex_file.py
@@ -8,7 +8,7 @@ def overlize(s, is_path=False):
 
 include_paths = ["./src", "./lib"]
 include_extensions = ["*.py", "*.ini"]
-exclude = ["NoCommit_", "\\old\\"]
+exclude = ["NoCommit_", "\\old\\", "__init__"]
 
 files = [
     file
@@ -21,10 +21,14 @@ files = [
 root = Path(".")
 previous_parent_path = None
 with open("NoCommit_Latex_include_code.tex", "w") as fp:
+    fp.write("\\chapter{Code}\n\n")
+    fp.write("""\section{Project root}
+\subsection*{config.ini}
+\inifile{../../../config.ini}""")
     for file in files:
         path = file.relative_to(root)
         parent_path = path.parent
         if parent_path != previous_parent_path:
             fp.write(f"""\\section{{{overlize(parent_path)}}}\n""")
             previous_parent_path = parent_path
-        fp.write(f"""\\subsection{{{overlize(path)}}}\n\\lstinputlisting{{../../../{overlize(path, is_path=True)}}}\n""")
+        fp.write(f"""\\subsection*{{{overlize(path)}}}\n\\pythonfile{{../../../{overlize(path, is_path=True)}}}\n""")
