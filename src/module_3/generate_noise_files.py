8812ca2ce69fa450b9596ab65deb2ccb7a9fddce
4rinze <augwoke@tudelft.nl>
2026-01-09 09:19:56 +0100

    Changes to generate_noise_files for phantom model test, 3d localization works with MUSIC doesn't with MVDR

--- src/module_3/generate_noise_files.py
+++ src/module_3/generate_noise_files.py
@@ -8,20 +8,25 @@ Fs = 48000
 signal1 = np.random.randint(-5000, 5000, 10*Fs).astype(np.int16).reshape(-1,1)
 signal2 = np.random.randint(-5000, 5000, 10*Fs).astype(np.int16).reshape(-1,1)
 signalsource1 = np.concatenate((signal1, signal2), axis=1)
-write("file_src1.wav", Fs, signalsource1)
+write("file_src1.wav", Fs, signalsource1) #double source wav file
 
 print (signalsource1.shape)
 print (signalsource1)
-print ("okay")
+print ("okay1")
 
 zeroed_s1 = np.zeros(len(signal1)).astype(np.int16).reshape(-1,1)
 signalsource2 = np.concatenate((zeroed_s1, signal2), axis =1)
 
 print (signalsource2.shape)
 print (signalsource2)
-print ("okay")
-write("file_src2.wav", Fs, signalsource2)
+print ("okay2")
+write("file_src2.wav", Fs, signalsource2) #single source wav file
 
+signalsource3 = np.concatenate((signal2, zeroed_s1), axis =1)
+print (signalsource3.shape)
+print (signalsource3)
+print ("okay3")
+write("file_src3.wav", Fs, signalsource3) #single source wav file
 
 
 #winsound.PlaySound("file_src1.wav", winsound.SND_FILENAME)
