b542b4e0e90f9be79dd60fc83cd319304045faef
Gerrald <gerrald@weelden.net>
2026-01-23 17:13:50 +0100

    Just doing a lot of things

--- src/localization_3D/launchTui.py
+++ src/localization_3D/launchTui.py
@@ -4,7 +4,7 @@ from lib.samples.Samples import SAMPLES_CALIBRATED, SAMPLES_SELF_CALIBRATED
 # sample = SAMPLES_CALIBRATED["Phantom"][0]
 # sample = SAMPLES_CALIBRATED["Generated"][0]
 # sample = SAMPLES_SELF_CALIBRATED["Phantom"][5]
-sample = SAMPLES_SELF_CALIBRATED["Real"][0]
+sample = SAMPLES_SELF_CALIBRATED["Real"][3]
 
 print(f"Using {sample.name}")
 
